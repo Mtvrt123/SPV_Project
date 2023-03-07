b4016e05f2bf7ecb16288a6cbbe02506a387d6eb
Mtvrt 123 <mlinaricnejc@gmail.com>
2023-03-07 14:57:09 +0100

    DB, + Get user

--- SPV_Project/Database.cs
+++ SPV_Project/Database.cs
@@ -5,14 +5,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using System.Web;
+
 namespace SPV_Project
 {
     public class Database
     {
+        private MySqlConnection conn;
+
+        public Database() 
+        {
+            string connStr = "server=localhost;user=root;database=mydb;port=3306;password=";
+            conn = new MySqlConnection(connStr);
+        }
+
         public Uporabnik GetUser()
         {
-            // Get user from database
-            return new Uporabnik(1, "ImeTest", "PriimekTest", "USERNAME", "EMAIL", "GESLO");
+
+            conn.Open();
+            MySqlCommand command = new MySqlCommand("Select * from uporabnik where uporabnik_ID=@id", conn);
+            command.Parameters.AddWithValue("@id", "1");
+
+            int result = command.ExecuteNonQuery();
+
+            Uporabnik uporabnik = null;
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    int id = (int)reader["uporabnik_ID"];
+                    string email = (string)reader["email"];
+                    string password = (string)reader["geslo"];
+
+                    uporabnik = new Uporabnik(id, "IME", "PRIIMEK", "USERNAME", email, password);
+                }
+
+            }
+
+            conn.Close();
+
+            return uporabnik;
         }
 
         public void UpdateUser(Uporabnik uporabnik)
