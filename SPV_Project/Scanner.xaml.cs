d2fb76a752d68ebd1d4cc0ae6fa2c01dcca46a50
Kristjan Herodež <kristjan.herodez@student.um.si>
2023-03-09 23:02:25 +0100

    Dodan Scanner

--- SPV_Project/Scanner.xaml.cs
+++ SPV_Project/Scanner.xaml.cs
@@ -6,4 +6,27 @@ public partial class Scanner : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    async void TakePhoto_Clicked(object sender, EventArgs e)
+    {
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+            if (photo != null)
+            {
+                // save the file into local storage
+                string localFilePath = System.IO.Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+
+                using Stream sourceStream = await photo.OpenReadAsync();
+                using FileStream localFileStream = File.OpenWrite(localFilePath);
+
+                await sourceStream.CopyToAsync(localFileStream);
+
+                var stream = await photo.OpenReadAsync();
+                PhotoImage.Source = ImageSource.FromStream(() => stream);
+
+            }
+        }
+    }
 }
